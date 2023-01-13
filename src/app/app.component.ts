9363d0c9e85f62f5dbbf78818426b41dd1945eee
Cristian <cristian.morales@thefuzzyfish.com>
2023-01-13 19:30:54 -0300

    some logic and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 
+type TUrlSaved = {
+  short: string;
+  original: string;
+  label: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  urlGenerated: string = '';
-  urlsSaved: string[] = [];
+  urlGenerated: string = 'shorturl.at/juIST';
+  urlsSaved: TUrlSaved[] = [];
 
   private isUrl(urlString: string) {
     const inputElement = document.createElement('input');
@@ -16,6 +22,7 @@ export class AppComponent {
 
     return inputElement.checkValidity();
   }
+
   generateShortUrl(urlString: string) {
     if (!this.isUrl(urlString)) return alert('Invalid url');
     //request to backend
@@ -23,8 +30,26 @@ export class AppComponent {
   }
 
   saveUrlGenerated() {
-    this.urlsSaved.push(this.urlGenerated);
+    const labelUrl = prompt('Please introduce a name for url: ');
+    if (!labelUrl) return;
+
+    const inputValue = (document.getElementById('input-url') as any).value;
+    this.urlsSaved.push({
+      short: this.urlGenerated,
+      original: inputValue,
+      label: labelUrl,
+    });
+
+    this.clear();
+  }
+
+  copyUrlGenerated() {
+    navigator.clipboard.writeText(this.urlGenerated);
+  }
+
+  clear() {
+    const inputUrlForm = document.getElementById('input-url-form') as any;
+    inputUrlForm.reset();
     this.urlGenerated = '';
-    console.log(this.urlsSaved);
   }
 }
